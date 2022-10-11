59d56c82b747b1f63805cb96f741a9325086e11f
Hasnain <mrajhasnain101@gmail.com>
2022-10-11 21:40:30 +0600

    js added accordingly

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
-class Calculator()
+class Calculator
 {
-    constructor(...Calculator,...Calculator.)
+    constructor(previousOperandTxt,currentOperandTxt){
+        this.currentOperandTxt=currentOperandTxt;
+        this.previousOperandTxt=previousOperandTxt;
+        this.clear();
+    }
+    clear(){
+         this.currentOperand="";
+         this.previousOperand="";
+         this.operation=undefined;
+    }
+
+    appendNumbers(numbers){
+        this.currentOperand=numbers;
+    }
+
+    chooseOperations(operation)
+    {
+
+    } 
+
+    computeNumbers(){
+
+    }
+
+    updateDisplay(){
+        this.currentOperandTxt.innerText=this.currentOperand;
+    }
+
 }
-
-
-
-
-
 
 const numbers=document.querySelectorAll("[data-number]");
 const operators=document.querySelectorAll("[data-op]");
 const deleteBtn=document.querySelector("[data-delete]");
 const clearBtn=document.querySelector("[data-clear]");
 const equals=document.querySelector("[data-equal]");
+const previousOperandTxt=document.querySelector("[data-previous-operand]");
+const currentOperandTxt=document.querySelector("[data-current-operand]");
 
+const calculator=new Calculator(previousOperandTxt,currentOperandTxt);
+numbers.forEach(button =>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumbers(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
