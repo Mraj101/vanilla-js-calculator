e11f1bc6caf6f215239485e88cf11853028d361b
Hasnain <mrajhasnain101@gmail.com>
2022-10-11 20:42:46 +0600

    added variables

---
+++ js/script.js
@@ -0,0 +1,17 @@
+class Calculator()
+{
+    constructor(...Calculator,...Calculator.)
+}
+
+
+
+
+
+
+const numbers=document.querySelectorAll("[data-number]");
+const operators=document.querySelectorAll("[data-op]");
+const deleteBtn=document.querySelector("[data-delete]");
+const clearBtn=document.querySelector("[data-clear]");
+const equals=document.querySelector("[data-equal]");
+
+
